176a943d23e252e5d52b49b79e969b481dcdf8b6
ian-afk <68414797+ian-afk@users.noreply.github.com>
2024-04-25 17:59:22 +0800

    fix type error warning  of handleChange event

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,17 +1,17 @@
 import { useState } from "react";
 
 function TodoForm() {
-  const [todo, setTodo] = useState();
+  const [todo, setTodo] = useState("");
 
-  function handleChange(e: HTMLInputElement) {
-    const { name, value } = e.target;
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setTodo(e.target.value);
   }
 
   return (
     <>
       <form action="#">
         <input type="text" value={todo} onChange={handleChange} />
-        <input type="submit" />
+        <button>Add</button>
       </form>
     </>
   );
