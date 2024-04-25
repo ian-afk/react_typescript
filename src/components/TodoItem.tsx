0962e43814cc2f8d8cc236700974fbedaf506ec1
ian-afk <68414797+ian-afk@users.noreply.github.com>
2024-04-25 18:13:49 +0800

    move the TodoItem function into a component

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,11 @@
+export default function TodoItem() {
+  return (
+    <>
+      <label htmlFor="#">
+        <input type="checkbox" />
+        <span>Hello</span>
+      </label>
+      <button>Delete</button>
+    </>
+  );
+}
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,19 +1,9 @@
+import TodoItem from "./TodoItem";
+
 export default function TodoList() {
   return (
     <div>
       <TodoItem />
     </div>
-  );
-}
-
-function TodoItem() {
-  return (
-    <>
-      <label htmlFor="#">
-        <input type="checkbox" />
-        <span>Hello</span>
-      </label>
-      <button>Delete</button>
-    </>
   );
 }
