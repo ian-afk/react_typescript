b61baa01bc0aa566dfd7efc27e9cf019dc80d321
ian-afk <68414797+ian-afk@users.noreply.github.com>
2024-04-26 13:05:48 +0800

    add sort functionality for completed todo and conditional rendering if no item

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -13,16 +13,32 @@ export default function TodoList({
   onCompletedChange,
   onDeleteTodo,
 }: TodoListProps) {
+  const todoSort = todo.sort((a, b) => {
+    if (a.completed === b.completed) {
+      return b.id - a.id;
+    } else {
+      return a.completed ? 1 : -1;
+    }
+  });
+
+  console.log(todoSort);
+
   return (
     <div className={styles.todoList}>
-      {todo.map((item) => (
-        <TodoItem
-          onDeleteTodo={onDeleteTodo}
-          todo={item}
-          key={item.id}
-          onCompletedChange={onCompletedChange}
-        />
-      ))}
+      {todo.length > 0 ? (
+        todoSort.map((item) => (
+          <TodoItem
+            onDeleteTodo={onDeleteTodo}
+            todo={item}
+            key={item.id}
+            onCompletedChange={onCompletedChange}
+          />
+        ))
+      ) : (
+        <p style={{ fontSize: "2.5rem", margin: "0 auto" }}>
+          No todo. Add new todo.
+        </p>
+      )}
     </div>
   );
 }
