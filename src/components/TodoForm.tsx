5d2a560c9057453aea12d85ad3c7e346f77060c8
ian-afk <68414797+ian-afk@users.noreply.github.com>
2024-04-26 13:44:50 +0800

    set limitation or add condition for adding todos. limit 10

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import styles from "./TodoForm.module.css";
+import { TodoForm as todo } from "../models/todoForm";
 
 interface TodoFormProps {
   onAddTodo: (title: string) => void;
+  todos: todo[];
 }
 
-function TodoForm({ onAddTodo }: TodoFormProps) {
+function TodoForm({ onAddTodo, todos }: TodoFormProps) {
   const [todo, setTodo] = useState("");
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
@@ -14,6 +16,16 @@ function TodoForm({ onAddTodo }: TodoFormProps) {
 
   function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
+
+    const completedTodo = todos.filter((item) => item.completed !== true);
+
+    // const completedTodo = todos.reduce((acc, curr) => {
+    //   if (curr.completed !== true) return acc + curr;
+    // }, 0);
+    console.log(completedTodo.length);
+
+    if (completedTodo.length >= 10)
+      return alert("You hit the limit, please complete 10 todos first!");
     if (!todo) return;
     onAddTodo(todo);
     setTodo("");
